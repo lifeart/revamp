3862b66f3e1b3bed28c9b192801cc561d5c3119c
Alex Kanunnikov <lifeart92@gmail.com>
2025-11-30 19:09:17 +0300

    +

--- src/transformers/polyfills/element.ts
+++ src/transformers/polyfills/element.ts
@@ -81,4 +81,86 @@ export const elementPolyfill = `
   if (typeof HTMLCollection !== 'undefined' && HTMLCollection.prototype && !HTMLCollection.prototype.forEach) {
     HTMLCollection.prototype.forEach = Array.prototype.forEach;
   }
+
+  // Element.prototype.scrollTo polyfill (Safari 9 doesn't have it)
+  if (typeof Element !== 'undefined' && !Element.prototype.scrollTo) {
+    Element.prototype.scrollTo = function(optionsOrX, y) {
+      if (typeof optionsOrX === 'object') {
+        // Options object: { top, left, behavior }
+        var left = optionsOrX.left !== undefined ? optionsOrX.left : this.scrollLeft;
+        var top = optionsOrX.top !== undefined ? optionsOrX.top : this.scrollTop;
+        this.scrollLeft = left;
+        this.scrollTop = top;
+      } else if (typeof optionsOrX === 'number') {
+        // Legacy: scrollTo(x, y)
+        this.scrollLeft = optionsOrX;
+        this.scrollTop = y !== undefined ? y : this.scrollTop;
+      }
+    };
+  }
+
+  // Element.prototype.scrollBy polyfill
+  if (typeof Element !== 'undefined' && !Element.prototype.scrollBy) {
+    Element.prototype.scrollBy = function(optionsOrX, y) {
+      if (typeof optionsOrX === 'object') {
+        var left = optionsOrX.left !== undefined ? optionsOrX.left : 0;
+        var top = optionsOrX.top !== undefined ? optionsOrX.top : 0;
+        this.scrollLeft += left;
+        this.scrollTop += top;
+      } else if (typeof optionsOrX === 'number') {
+        this.scrollLeft += optionsOrX;
+        this.scrollTop += (y !== undefined ? y : 0);
+      }
+    };
+  }
+
+  // Element.prototype.scrollIntoView enhancement for older browsers
+  // (basic scrollIntoView exists, but options object is not supported)
+  (function() {
+    if (typeof Element === 'undefined') return;
+    var original = Element.prototype.scrollIntoView;
+    Element.prototype.scrollIntoView = function(optionsOrAlignToTop) {
+      if (typeof optionsOrAlignToTop === 'object' && optionsOrAlignToTop !== null) {
+        // Options object not fully supported in old browsers, fallback to basic behavior
+        var alignToTop = optionsOrAlignToTop.block === 'start' || optionsOrAlignToTop.block === undefined;
+        original.call(this, alignToTop);
+      } else {
+        original.call(this, optionsOrAlignToTop);
+      }
+    };
+  })();
+
+  // window.scrollTo polyfill (should exist but ensure options object support)
+  (function() {
+    if (typeof window === 'undefined') return;
+    var originalWindowScrollTo = window.scrollTo;
+    window.scrollTo = function(optionsOrX, y) {
+      if (typeof optionsOrX === 'object' && optionsOrX !== null) {
+        var left = optionsOrX.left !== undefined ? optionsOrX.left : window.pageXOffset;
+        var top = optionsOrX.top !== undefined ? optionsOrX.top : window.pageYOffset;
+        originalWindowScrollTo.call(window, left, top);
+      } else {
+        originalWindowScrollTo.call(window, optionsOrX, y);
+      }
+    };
+    // Also alias scroll to scrollTo
+    if (!window.scroll || window.scroll === originalWindowScrollTo) {
+      window.scroll = window.scrollTo;
+    }
+  })();
+
+  // window.scrollBy polyfill with options object support
+  (function() {
+    if (typeof window === 'undefined') return;
+    var originalWindowScrollBy = window.scrollBy;
+    window.scrollBy = function(optionsOrX, y) {
+      if (typeof optionsOrX === 'object' && optionsOrX !== null) {
+        var left = optionsOrX.left !== undefined ? optionsOrX.left : 0;
+        var top = optionsOrX.top !== undefined ? optionsOrX.top : 0;
+        originalWindowScrollBy.call(window, left, top);
+      } else {
+        originalWindowScrollBy.call(window, optionsOrX, y);
+      }
+    };
+  })();
 `;
